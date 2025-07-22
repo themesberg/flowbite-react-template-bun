4dfdcbd1f1c5a912dfae1bac6220e2f5a2b03151
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:18:13 +0300

    up packages

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -5,13 +5,21 @@
  * It is included in `src/index.html`.
  */
 
+import { ThemeInit } from ".flowbite-react/init";
+import { initThemeMode } from "flowbite-react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 
 function start() {
   const root = createRoot(document.getElementById("root")!);
-  root.render(<App />);
+  root.render(
+    <>
+      <ThemeInit />
+      <App />
+    </>,
+  );
+  initThemeMode();
 }
 
 if (document.readyState === "loading") {
